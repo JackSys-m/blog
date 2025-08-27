ab12847708e714e855461c4613a116714863b63b
JackSys-m <jack1qj1@gmail.com>
2025-08-27 23:08:32 +0300

    Definition model BlogPost

--- blogs/models.py
+++ blogs/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class BlogPost(models.Model):
+    """Блог, который ведёт пользователь."""
+    text = models.CharField(max_length=350)
+    date_added = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        """Возвращает строковое представление модели."""
+        return self.text
