55199d6a1640c874da4cc8131963df8f21e0d5af
JackSys-m <jack1qj1@gmail.com>
2025-08-28 23:22:20 +0300

    Definition model Entry

--- blogs/models.py
+++ blogs/models.py
@@ -8,3 +8,19 @@ class BlogPost(models.Model):
     def __str__(self):
         """Возвращает строковое представление модели."""
         return self.text
+
+class Entry(models.Model):
+    """Информация, представленная пользователем в блоге."""
+    post = models.ForeignKey(BlogPost, on_delete=models.CASCADE)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        verbose_name_plural = 'entries'
+    
+    def __str__(self):
+        """Возвращает строковое представление модели."""
+        if len(self.text) < 101:
+            return self.text
+        else:
+            return f"{self.text[:100]}..."
