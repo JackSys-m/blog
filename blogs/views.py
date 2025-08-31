b2d6be3edabe3723ee35e31934c56951d256e541
JackSys-m <jack1qj1@gmail.com>
2025-08-31 22:40:40 +0300

    Create view for blogs page

--- blogs/views.py
+++ blogs/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from .models import BlogPost
 
 def index(request):
     """Домашняя страница приложения Blogs."""
     return render(request, 'blogs/index.html')
+
+def blogs(request):
+    """Выводит перечень блогов."""
+    blogs = BlogPost.objects.order_by('date_added')
+    context = {'blogs': blogs}
+    return render(request, 'blogs/blogs.html', context)
