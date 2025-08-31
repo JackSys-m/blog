4e627510622421729f48feae8ab08d0680e17065
JackSys-m <jack1qj1@gmail.com>
2025-08-31 22:31:03 +0300

    Determine URL for blogs page

--- blogs/urls.py
+++ blogs/urls.py
@@ -7,4 +7,6 @@ app_name = 'blogs'
 urlpatterns = [
     # Домашняя страница
     path('', views.index, name='index'),
+    # Страница со списком всех блогов
+    path('blogs/', views.blogs, name='blogs'),
 ]
