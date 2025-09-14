125a5f18f60e62b95fe22c74bb7376ccf7ce7b11
JackSys-m <jack1qj1@gmail.com>
2025-09-14 23:31:33 +0300

    Determine URL for new_entry

--- blogs/urls.py
+++ blogs/urls.py
@@ -12,4 +12,6 @@ urlpatterns = [
     # Страница с подробной информацией по отдельному блогу
     path('blogs/<int:blog_id>/', views.blog, name='blog'),
     path('new_blog/', views.new_blog, name='new_blog'),
+    # Страница для добавления новой записи
+    path('new_entry/<int:blog_id>', views.new_entry, name='new_entry'),
 ]
